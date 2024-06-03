a4973117bf87f21a1cc46bba7f3a299a2347db9e
SvitlanaKliuchar <114626427+SvitlanaKliuchar@users.noreply.github.com>
2024-06-03 20:07:05 -0300

    Set up layouts and routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// Import the layouts
+import RootLayout from './layouts/root-layout';
+import MainLayout from './layouts/main-layout';
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
+// Import the components
+import IndexPage from './routes';
+import ContactPage from './routes/contact';
+import SignInPage from './routes/sign-in';
+import SignUpPage from './routes/sign-up';
+import MainPage from './routes/main';
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/contact", element: <ContactPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <MainLayout />,
+        path: "main",
+        children: [
+          { path: "/main", element: <MainPage /> },
+          // { path: "/dashboard/invoices", element: <InvoicesPage /> }
+        ]
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
