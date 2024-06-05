49ab424a116bce6442359bd6b4eb11462101ca77
SvitlanaKliuchar <114626427+SvitlanaKliuchar@users.noreply.github.com>
2024-06-04 22:02:27 -0300

    Improve css

--- client/src/components/homepage/send-note.jsx
+++ client/src/components/homepage/send-note.jsx
@@ -3,7 +3,7 @@ export default function SendNote() {
     return (<>
     <div className="send-note-container">
         <form /* onSubmit={handleSubmit} */>
-            <label className="label">Write a note to your friend</label>
+            <label className="label">Send a note to a friend</label>
             <input type="text" id="email" name="email" placeholder="email:" required />
             <textarea name="note" id="note" rows={5} cols={25} required />
             <button type="submit" className="btn send-btn">send</button>
